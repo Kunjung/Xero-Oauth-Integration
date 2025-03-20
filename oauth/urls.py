47c32afbe410d7b61a9e86f3bda7a80ce175a24a
Kunjung <kunjungsherpa@gmail.com>
2025-03-20 13:34:03 +0545

    added route for refresh token

--- oauth/urls.py
+++ oauth/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('authorize/', views.authorize, name='authorize'),
     path('callback/', views.callback, name='callback'),
+    path('refresh/', views.refresh_access_token, name='refresh'),
     path('account/', views.get_xero_data, name='account'),
 ]
